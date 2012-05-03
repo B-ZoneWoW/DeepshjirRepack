7ae9769d92fa88c009dc01d5a628c17ce39a039c
PaladMaster <arkaniaark@yahoo.com>
2012-05-03 22:47:27 +0430

    Update src/server/scripts/Kalimdor/LostCityOfTheTolvir/boss_siamat.cpp

--- src/server/scripts/Kalimdor/LostCityOfTheTolvir/boss_siamat.cpp
+++ src/server/scripts/Kalimdor/LostCityOfTheTolvir/boss_siamat.cpp
@@ -1,136 +1,85 @@
-#include "ScriptPCH.h"
+/*
+ * Copyright (C) 2010 - 2012 ProjectSkyfire <http://www.projectskyfire.org/>
+ * 
+ * Copyright (C) 2011 - 2012 ArkCORE <http://www.arkania.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#define spell_eclair 73564
-#define spell_vent 83066 
-
-#define SAY_AGGRO ""
-#define SAY_DIED ""
-
-class boss_siamat: public CreatureScript
+#include"ScriptPCH.h"
+#include"WorldPacket.h"
+#include"lost_city_of_the_tolvir.h"
+enum Spells 
 {
-public: 
-    boss_siamat() : CreatureScript("boss_siamat") { } 
+    SPELL_DEFLECT_SHIELD                          = 84589,
+    SPELL_STORM_BOLT                              = 73564,
+    SPELL_ABSSORB_STORM                           = 83151,
+    SPELL_SUMMON_CLOUD_BURST                      = 83790,
+    // this adds
+    SPELL_DEPLETION                               = 84550,
+    SPELL_CHAIN_LIGHTNING                         = 83455,
 
-    struct boss_siamatAI : public ScriptedAI
-    {
-        boss_siamatAI(Creature *c) : ScriptedAI(c) {}
+    SPELL_LIGHTNING_NOVA                          = 84544,
+    SPELL_THUNDER_CRASH                           = 84522,
+    SPELL_LIGHTINING_CHARGE                       = 91871,
 
-        uint32 eclair;
-        uint32 vent;
-
-        void Reset()
-        {
-            eclair = 5000; 
-            vent = 6000;		   
-        }
-
-        void EnterCombat(Unit* /*who*/) 
-        {
-            me->MonsterYell(SAY_AGGRO, LANG_UNIVERSAL, NULL);
-        }
-
-        void JustDied(Unit* /*killer*/)
-        {
-            me->MonsterYell(SAY_DIED, LANG_UNIVERSAL, NULL);
-        }
-
-        void UpdateAI(const uint32 diff)
-        {
-            if (!UpdateVictim())
-                return;
-
-            if (eclair <= diff)
-            {
-                if (Unit *pTarget = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    DoCast(pTarget, spell_eclair);
-                eclair = 5000;
-            }
-            else
-                eclair -= diff;
-
-            if (vent <= diff)
-            {
-                if (Unit *pTarget = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    DoCast(pTarget, spell_vent);
-                vent = 6000;
-            }
-            else
-                vent -= diff;
-
-            if (!UpdateVictim())
-                return;
-
-            DoMeleeAttackIfReady();
-        }
-    };
-
-    CreatureAI* GetAI(Creature* pCreature) const
-    {
-        return new boss_siamatAI (pCreature);
-    }
-
-};
-
-#define spell_fraca 84521
-#define spell_nova 84544
-
-class mob_siamat: public CreatureScript
-{
-public: 
-    mob_siamat() : CreatureScript("mob_siamat") { } 
-
-    struct mob_siamatAI : public ScriptedAI
-    {
-        mob_siamatAI(Creature *c) : ScriptedAI(c) {}
-
-        uint32 fraca;
-        uint32 nova;
-
-        void Reset()
-        {
-            fraca = 5000; 
-            nova = 15000;
-        }
-
-        void UpdateAI(const uint32 diff)
-        {
-            if (!UpdateVictim())
-                return;
-
-            if (fraca <= diff)
-            {
-                if (Unit *pTarget = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    DoCast(pTarget, spell_fraca);
-                fraca = 5000;
-            }
-            else
-                fraca -= diff;
-
-            if (nova <= diff)
-            {
-                if (Unit *pTarget = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    DoCast(pTarget, spell_nova);
-                nova = 15000;
-            }
-            else
-                nova -= diff;
-
-            if (!UpdateVictim())
-                return;
-
-            DoMeleeAttackIfReady();
-        }
-    };
-
-    CreatureAI* GetAI(Creature* pCreature) const
-    {
-        return new mob_siamatAI (pCreature);
-    }
-
-};
-
-void AddSC_boss_siamat() 
-{
-    new boss_siamat();
-    new mob_siamat();
+    SPELL_CLOUD_BURST_VISUAL                      = 83048,
+    SPELL_CLOUD_BURST                             = 83051,
 }
+
+enum Events 
+{
+    EVENT_DEFLECT_SHIELD = 1,
+    EVENT_STORM_BOLT,
+    EVENT_ABSSORB_STORM,
+    EVENT_SUMMON_CLOUD_BURST,
+    EVENT_SUMMON_MINION,
+    EVENT_SUMMON_SERVANT,
+};
+
+class boss_siamat: public CreatureScript 
+{
+public:
+    boss_siamat() : CreatureScript("boss_siamat") { }
+
+    CreatureAI* GetAI(Creature* pCreature) const 
+    {
+        return new boss_siamatAI(pCreature);
+    }
+
+    struct boss_siamatAI: public BossAI 
+    {
+        boss_siamatAI(Creature* pCreature) : BossAI(pCreature, DATA_SIAMAT_EVENT), Summons(me)
+        {
+            pInstance = pCreature->GetInstanceScript();
+        }
+
+        InstanceScript* pInstance;
+        EventMap events;
+        SummonList Summons;
+        uint8 servantCount;
+        uint8 servantDeaths;
+        uint8 phase;
+
+        void Reset() 
+        {
+            events.Reset();
+            Summons.DespawnAll();
+            servantCount = 0;
+            servantDeaths = 0;
+            phase = 1;
+
+            if (pInstance)
+                pInstance->SetData(DATA_SIAMAT_EVENT, NOT_STARTED);
+        }
